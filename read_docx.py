7bf777562786e4afeb672e235ec59a5da9d5c119
bearwangcai <1970299031@qq.com>
2019-10-30 15:22:51 +0800

    读取各专业评审意见 #写docx

--- read_docx.py
+++ read_docx.py
@@ -1,6 +1,4 @@
 import os  
-import xlrd
-import xlwt
 import docx
 from docx import Document
 
@@ -13,22 +11,16 @@ global content_name #共性意见 个性意见
 
 def file_name(file_dir):   
     files_name = []
-    province = []   
+    profession_file = []   
     for root, dirs, files in os.walk(file_dir): #读文件夹中的文件名（根目录，子文件夹目录，文件名）  
         for file in files:  
             
+            (filename,_) = os.path.splitext(file)
+            profession_file.append(filename)
             files_name.append(os.path.join(root, file)) #合成标准路径名
-            '''
-            if (file[:2] == '内蒙'):
-                province.append('内蒙古')
-            elif (file[:2] == '黑龙'):
-                province.append('黑龙江')
-            else:
-                province.append(file[:2])
-            '''
 
     num = len(files_name)              
-    return  files_name, num #都有哪些省，文件路径，文件总数
+    return  profession_file, files_name, num #都有哪些专业，文件路径，文件总数
 
 def read(file_name):
     province = {} #按省存储
@@ -42,31 +34,32 @@ def read(file_name):
             province[province_name_now][content_name_now] = [] #创建共性意见/个性意见字典
         else:
             try:
-                province[province_name_now][content_name_now].add(paragraph.text) #填写内容
+                province[province_name_now][content_name_now].append(paragraph.text) #填写内容
             except:
                 pass
     return province
 
 
-
-if __name__ == "__main__":
+def read_file():
     profession = {}
-    data0 = [] #全局变量，按行存储各省工作进展
-    data1 = [] #全局变量，按行存储各省需集团协调问题
     global province_name #省名
     province_name = ['北京','天津','河北','山西','内蒙古','辽宁','吉林','黑龙江','上海','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','重庆','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆']
     global content_name #共性意见 个性意见
     content_name = ['（一）共性意见','（二）你省审查意见']
-    unopen_province = [] #全局变量，打不开文件的省
     file_dir = r'C:\Users\x270\Desktop\新建文件夹'
-    files_name, num = file_name(file_dir)
+    profession_file, files_name, num = file_name(file_dir)
 
     print(num) #文件总数
 
     for i in range(num): #读文件
-        profession['IDC'] = read(files_name[i])
+        profession[profession_file[i]] = read(files_name[i])
 
-    print('可以打开的文件总数',len(data0))
-    print('不可以打开的文件总数',len(unopen_province)) 
-    print('不可以打开文件的省',unopen_province)
+    return profession
+
+
+
+if __name__ == "__main__":
+    profession = read_file()
+
+
 
